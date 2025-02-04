7426a0f48f81cb8e82e7c9f367a65b4967bea30f
Awani Caleb <calebawani@gmail.com>
2025-02-04 11:53:00 -0800

    Final changes and personalisation

---
+++ src/assets/her/index.js
@@ -0,0 +1,5 @@
+export { default as first } from "./first.jpg";
+export { default as second } from "./second.jpg";
+export { default as third } from "./third.jpg";
+export { default as forth } from "./forth.jpg";
+export { default as fifth } from "./fifth.jpg";
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -29,56 +29,61 @@ function Card() {
 
   return (
     <div className="min-h-screen flex flex-col items-center justify-center overflow-clip">
-    <div className="w-[400px]  h-screen flex flex-col items-center justify-center">
-      <motion.div  initial={{ opacity: 0, visibility: "hidden" }}
+      <div className="w-[400px]  h-screen flex flex-col items-center justify-center">
+        <motion.div
+          initial={{ opacity: 0, visibility: "hidden" }}
           animate={{ opacity: 1, visibility: "visible" }}
-          transition={{duration: 1.2}}>
-      <div id="card" className={`${cardClass}`} onClick={toggleCard}>     
-        <div id="card-inside">
-          <div className="wrap">
-            <p>Happy Birthday, John Doe!</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque delectus dolore recusandae eveniet dicta. Corrupti.</p>
-            <p>
-              I hope you have a very blessed birthday!
-            </p>
-            <p>
-             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, cum.
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            </p>
-            <p className="signed">Jane Doe</p>
+          transition={{ duration: 1.2 }}
+        >
+          <div id="card" className={`${cardClass}`} onClick={toggleCard}>
+            <div id="card-inside">
+              <div className="wrap">
+                <p>Happy Birthday, my love!</p>
+                <p>
+                  Today I celebrate not just your birthday, but every moment you
+                  make my life brighter.
+                </p>
+                <p>
+                  I hope your day is filled with the same warmth and joy you
+                  bring into my world.
+                </p>
+                <p>
+                  Every smile, every laugh, every little thing about you
+                  inspires me to be a better person.
+                </p>
+                <p>
+                  Thank you for being you. Amazing, inspiring, and absolutely
+                  irreplaceable. I love you so much mama!
+                </p>
+                <p className="signed">Awani Caleb</p>
+              </div>
+            </div>
+
+            <div id="card-front">
+              <div className="wrap">
+                <h1>Happy Birthday!</h1>
+              </div>
+            </div>
           </div>
-        </div>
-
-        <div id="card-front">
-          <div className="wrap">
-            <h1>Happy Birthday!</h1>
-          </div>
-        </div>
-    </div>
-
-      </motion.div>
-
-    {/* prone to bugs */}
-      {isCardOpened && (
-        <motion.div className="-mt-[3rem]" initial={{ opacity: 0, visibility: "hidden" }}
-        animate={{ opacity: 1, visibility: "visible" }}
-        transition={{duration: 1.2}}> 
-        <Link to ='/cake'>
-        <p className="-mt-[4rem] px-7 py-3 bg-customBlue text-white font-medium text-base rounded-full hover:bg-blue-600">
-            Next Page
-          </p>
-        </Link>
-            
         </motion.div>
-         
-        )}
 
+        {/* prone to bugs */}
+        {isCardOpened && (
+          <motion.div
+            className="-mt-[3rem]"
+            initial={{ opacity: 0, visibility: "hidden" }}
+            animate={{ opacity: 1, visibility: "visible" }}
+            transition={{ duration: 1.2 }}
+          >
+            <Link to="/cake">
+              <p className="-mt-[4rem] px-7 py-3 bg-customBlue text-white font-medium text-base rounded-full hover:bg-blue-600">
+                Next Page
+              </p>
+            </Link>
+          </motion.div>
+        )}
+      </div>
     </div>
-    
-    </div>
-    
   );
 }
 
